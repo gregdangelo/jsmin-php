722c1d3095dddf14bf7919b51bbb928922581a3e
Greg dangelo <gregdangelo@gmail.com>
2012-02-06 14:20:26 -0500

    comment updates - minor

--- jsmin.php
+++ jsmin.php
@@ -41,8 +41,9 @@
  * @copyright 2002 Douglas Crockford <douglas@crockford.com> (jsmin.c)
  * @copyright 2008 Ryan Grove <ryan@wonko.com> (PHP port)
  * @license http://opensource.org/licenses/mit-license.php MIT License
- * @version 1.1.1 (2008-03-02)
+ * @version 1.1.2 (2008-03-02)
  * @link https://github.com/rgrove/jsmin-php/
+ * @link https://github.com/gregdangelo/jsmin-php
  */
 
 class JSMin {
